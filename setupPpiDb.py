84a187cd3520dadcda1d67efc8ebae947a7b261c
sagagugit <122979609+sagagugit@users.noreply.github.com>
2023-08-01 21:35:49 +0530

    Update setupPpiDb.py

--- setupPpiDb.py
+++ setupPpiDb.py
@@ -754,6 +754,9 @@ def calcEnergyTerms(pdbsToAnalyze):
             new_df["equal"] = True_False
             result= new_df.loc[new_df['equal']==True]
             result_2=result.groupby('Chain', group_keys=False).apply(lambda x: x.loc[x.distance_x.idxmin()])
+            result_2 = result_2.iloc[:, [1, 2]]
+            result_2 = result_2.assign(Chain=result_2['Residue'].str[3:-1], Residue_number=result_2['Residue'].str[-1:])
+            result_2.drop(columns=['Residue'], inplace=True)
             result_2.to_csv('results/cold_spots/dis.csv', sep=str(','), header=True)
         except:
             pass

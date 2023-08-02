11c6bc0f422d795c9d76a4d3530e3eb42195fcc0
sagagugit <122979609+sagagugit@users.noreply.github.com>
2023-08-02 12:49:23 +0530

    Update setupPpiDb.py

--- setupPpiDb.py
+++ setupPpiDb.py
@@ -864,9 +864,9 @@ def calcEnergyTerms(pdbsToAnalyze):
             line_file4 = f4.read()
             print(line_file2,'\n',line_file3,'\n',line_file1,'\n',line_file4,'\n', file=cold_file3)
 
-   #         os.remove(os.path.join(RESULTS_DIR, COLD_SPOTS_DIR, "No_of_cold_spots_due_to_cavities.csv"))
-  #          os.remove(os.path.join(RESULTS_DIR, COLD_SPOTS_DIR, "Cold_spots_residues_due_to_CH_interactions.csv"))
- #           os.remove(os.path.join(RESULTS_DIR, COLD_SPOTS_DIR, "Cold_spots_due_to_SC_interactions.csv"))
+            os.remove(os.path.join(RESULTS_DIR, COLD_SPOTS_DIR, "No_of_cold_spots_due_to_cavities.csv"))
+            os.remove(os.path.join(RESULTS_DIR, COLD_SPOTS_DIR, "Cold_spots_residues_due_to_CH_interactions.csv"))
+            os.remove(os.path.join(RESULTS_DIR, COLD_SPOTS_DIR, "Cold_spots_due_to_SC_interactions.csv"))
 
 
     if os.path.exists(os.path.join(RESULTS_DIR, PARAMETERS_DIR, 'atom_depths.csv')):
